7959f03e262677b56bac938201e94ef8c6c6f04c
BrennanTanner <brenntanner@gmail.com>
2023-01-05 19:30:00 -0700

    Prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your Grade percentage?: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letterGrade;
+        string sign;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (letterGrade != "A" &&  letterGrade != "F") 
+        {
+            if (7 <= grade % 10) 
+            {
+                sign = "+";
+            }          
+            else if (3 >= grade % 10) 
+            {
+                sign = "-";
+            }
+            else 
+            {
+               sign = ""; 
+            }
+
+            
+        }
+        else 
+        {
+            sign = ""; 
+        }
+
+        Console.Write($"Your Grade is: {letterGrade}{sign}");
+
+        if (letterGrade == "D" ||  letterGrade == "F") 
+        {
+            Console.Write(", you failed. Don't be sorry, be better.");
+        }
+        else
+        {
+            Console.Write(", wow! I can't believe you passed.");
+        }
     }
 }

db8b890ae11407e7d3dea970a426eec2c2982533
BrennanTanner <brenntanner@gmail.com>
2023-01-11 22:31:36 -0700

    Learning2 Complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Apple Eater";
+        job1._startYear = "1843";
+        job1._endYear = "1846";
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Computer Man";
+        job2._startYear = "1943";
+        job2._endYear = "1946";
+
+        Resume myResume = new Resume();
+        myResume._name = "Bradley Tanner";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }

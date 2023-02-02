793a64717ea289df58f7440bf5909083c1a9e134
BrennanTanner <brenntanner@gmail.com>
2023-02-01 19:25:07 -0700

    Learning05 Finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square( "Red", 18.5));
+        shapes.Add(new Rectangle( "Blue", 12.6, 21.9));
+        shapes.Add(new Circle( "Green", 13.3));
+
+        int index = 0;
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape {index + 1}");
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}\n");
+            index++;
+        }
+
+       
     }
 }
